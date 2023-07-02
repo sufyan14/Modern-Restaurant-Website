1d0d2df5792d657fda0ac15c7a021841b802b54e
Muhammad Sufyan <muhammadsufyan00ms@gmail.com>
2023-07-02 21:35:08 +0500

    done

--- restaurant/src/container/Footer/Footer.jsx
+++ restaurant/src/container/Footer/Footer.jsx
@@ -1,10 +1,52 @@
-import React from 'react';
+import React from "react";
 
-import './Footer.css';
+import { FiFacebook, FiTwitter, FiInstagram } from "react-icons/fi";
+import { FooterOverlay, Newsletter } from "../../components";
+import { images } from "../../constants";
+import "./Footer.css";
 
 const Footer = () => (
-  <div>
-    Footer
+  <div className="app__footer section__padding">
+    <FooterOverlay />
+    <Newsletter />
+
+    <div className="app__footer-links">
+      <div className="app_footer-links_contact">
+        <h1 className="app__footer-headtext">Contact Us</h1>
+        <p className="p__opensans">
+          DHA Phase 6 12th St, Karachi, Pakistan.
+        </p>
+        <p className="p__opensans">(021) 54612854</p>
+        <p className="p__opensans">(021) 77412850</p>
+      </div>
+      <div className="app__footer-links_logo">
+        <img src={images.gustogrill} alt="footer_logo" />
+        <p className="p__opensans">
+          "The best way to find yourself is to lose yourself in the service of
+          others"
+        </p>
+        <img
+          src={images.spoon}
+          className="spoon__img"
+          style={{ marginTop: 15 }}
+        />
+        <div className="app__footer-links_icons">
+          <FiFacebook />
+          <FiTwitter />
+          <FiInstagram />
+        </div>
+      </div>
+      <div className="app__footer-links_work">
+        <h1 className="app__footer-headtext">Working Hours</h1>
+        <p className="p__opensans">Monday-Friday:</p>
+        <p className="p__opensans">9:00 am - 12:00 am</p>
+        <p className="p__opensans">Saturday-Sunday:</p>
+        <p className="p__opensans">8:00 am - 10:00 pm</p>
+      </div>
+    </div>
+    <div className="footer_copyright">
+      <p className="p__opensans">2023 Gusto Grill. All Rights Reserved.</p>
+    </div>
   </div>
 );
 
